cf4876e186ed933d0036317907defbb24f1d20b7
PargevS <sargsyanpargev1991@gmail.com>
2021-04-04 17:24:43 +0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.scss';
+import LP_Chat from "./chat/LP_Chat/LP_Chat";
+
+function App() {
+  return (
+    <div className="app">
+      <LP_Chat/>
+    </div>
+  );
+}
+
+export default App;
